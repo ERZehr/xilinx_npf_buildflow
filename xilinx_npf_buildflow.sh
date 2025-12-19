28af7452a99dec7d2c714d2afe638a83bc7b79fc
ERZehr <139022059+ERZehr@users.noreply.github.com>
2025-12-19 17:51:51 -0500

    Refactor xilinx_npf_buildflow.sh for improved paths
    Updated paths for configuration management and synthesis scripts, and adjusted file movements for output logs and images.

--- xilinx_npf_buildflow.sh
+++ xilinx_npf_buildflow.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 set -e
-mkdir spnr img logs
-source ./cfg.sh
+
+# create build directories
+mkdir -p "$BUILD_DIR"
+mkdir -p "$IMG_DIR"
+mkdir -p "$LOG_DIR"
+
+# Source config from the script directory
+source "$TARGET_DIR/cfg.sh"
 
 # Collect version info
-../../modules/xilinx_npf_buildflow/configuration_managment.sh | tee logs/configuration_management.log
+"$NPF_DIR/configuration_managment.sh" | tee "$LOG_DIR/configuration_management.log"
 
 # Read files and run synthesis
-../../modules/xilinx_npf_buildflow/synthesis.sh | tee logs/synthesis.log
-mv *.dcp spnr/
+"$NPF_DIR/synthesis.sh" | tee "$LOG_DIR/synthesis.log"
+mv "$TARGET_DIR/*.dcp" "$BUILD_DIR/" || true
 
 # Implementation & Bitgen
-../../modules/xilinx_npf_buildflow/implement.sh | tee logs/implement.log
-mv *.dcp spnr/
-mv *.rpt logs/
-mv *.txt logs/
-mv *.bit img/
-mv *.bin img/
-mv *.mcs spnr/
-
+"$NPF_DIR/implement.sh" | tee "$TARGET_DIR/logs/implement.log"
+mv "$TARGET_DIR"/*.dcp "$BUILD_DIR" || true
+mv "$TARGET_DIR"/*.mcs "$BUILD_DIR" || true
+mv "$TARGET_DIR"/*.rpt "$LOG_DIR" || true
+mv "$TARGET_DIR"/*.txt "$LOG_DIR" || true
+mv "$TARGET_DIR"/*.bit "$IMG_DIR" || true
+mv "$TARGET_DIR"/*.bin "$IMG_DIR" || true
 
 # Format output files
